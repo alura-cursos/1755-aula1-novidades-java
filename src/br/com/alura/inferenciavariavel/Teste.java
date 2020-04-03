6fb0728ca496434fec34c3be91aca4582831dff4
João Victor <jvc.martins12@gmail.com>
2020-04-03 16:52:40 -0300

    Criação do teste de inferência de variáveis

---
+++ src/br/com/alura/inferenciavariavel/Teste.java
@@ -0,0 +1,14 @@
+package br.com.alura.inferenciavariavel;
+
+import java.util.HashMap;
+
+public class Teste {
+
+	//Novidade do Java 10 - Inferência de variável.
+	public static void main(String[] args) {
+		var cpfPorNomes = new HashMap<String, String>();
+		cpfPorNomes.put("João", "04813189");
+
+		System.out.println(cpfPorNomes);
+	}
+}
