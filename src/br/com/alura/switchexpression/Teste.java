58701c27455bf8a4bb8034d6d2b8faf04457b23d
João Victor <jvc.martins12@gmail.com>
2020-04-03 16:59:24 -0300

    Criação do teste para switch expressions

---
+++ src/br/com/alura/switchexpression/Teste.java
@@ -0,0 +1,14 @@
+package br.com.alura.switchexpression;
+
+public class Teste {
+
+	//Novidade do Java 14 - Switch Expressions.
+	public static void main(String[] args) {
+		String nome = "João";
+		switch (nome) {
+		case "Renata" -> System.out.println("Acertou: " + nome);
+		case "João" -> System.out.println("Acertou: " + nome);
+		default -> System.out.println("Nenhum nome encontrado!!");
+		}
+	}
+}
