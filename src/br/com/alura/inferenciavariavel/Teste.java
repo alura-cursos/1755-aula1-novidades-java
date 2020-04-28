a19420f792bb10ec7daa78483e9f83a795775d70
João Victor <jvc.martins12@gmail.com>
2020-04-28 15:27:01 -0300

    Arquivos após aplicação dos novos recursos

--- src/br/com/alura/inferenciavariavel/Teste.java
+++ src/br/com/alura/inferenciavariavel/Teste.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Teste {
 
 	public static void main(String[] args) {
-		HashMap<String, String> cpfPorNomes = new HashMap<String, String>();
+		var cpfPorNomes = new HashMap<String, String>();
 		cpfPorNomes.put("João", "04813189");
 
 		System.out.println(cpfPorNomes);
