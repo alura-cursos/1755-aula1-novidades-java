a19188e5c861d425d83ff2a4232b2ec327c17e2b
João Victor <jvc.martins12@gmail.com>
2020-04-28 15:20:32 -0300

    Arquivos da primeira aula

--- src/br/com/alura/inferenciavariavel/Teste.java
+++ src/br/com/alura/inferenciavariavel/Teste.java
@@ -4,9 +4,8 @@ import java.util.HashMap;
 
 public class Teste {
 
-	//Novidade do Java 10 - Inferência de variável.
 	public static void main(String[] args) {
-		var cpfPorNomes = new HashMap<String, String>();
+		HashMap<String, String> cpfPorNomes = new HashMap<String, String>();
 		cpfPorNomes.put("João", "04813189");
 
 		System.out.println(cpfPorNomes);
